407607da35124d2c5a801b4852ce3134732faeee
Leonardo David R.M <leodar1234@gmail.com>
2023-05-07 23:10:19 +0200

    commit

---
+++ AlgebraProject/pointTest.ts
@@ -0,0 +1,9 @@
+import { Point } from "./point";
+
+let point : Point = new Point(4,6);
+
+console.log(point.getX());
+console.log(point.getY());
+console.log(point.setX(9));
+console.log(point.setY(15));
+console.log(point.toString())
