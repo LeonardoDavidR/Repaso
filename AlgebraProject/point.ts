35acc8b0eb20588428aab50a58ce3cc81f991bba
Leonardo David R.M <leodar1234@gmail.com>
2023-05-09 21:25:12 +0200

    falta el ultimo

--- AlgebraProject/point.ts
+++ AlgebraProject/point.ts
@@ -74,11 +74,32 @@ export class Point {
         }
         return cuadrantes;
     }
+    calculateNearest(points : Point[]): Point{
+        let cercano : number = 100
+        for(let i = 0 ; i < points.length; i++){
+            
+            this.calculateDistance(points[i])
+        if(this.calculateDistance(points[i]) < cercano){
+                cercano = i
+        }
+    }
+    return points[cercano]
+    }
 }
+
+
 
 
 let point = new Point(4,-2);
 let point2 = new Point(4,4);
+let point3 = new Point(5,7);
+let point4 = new Point(4,8);
+let point5 = new Point(12,4);
+let points : Point []= [point3, point4,point5]
 
 console.log(point.calculateQuadrant())
+console.log(point.calculateDistance(point3))
+console.log(point.calculateDistance(point4));
+console.log(point.calculateDistance(point5))
 console.log(point.calculateDistance(point2))
+console.log(point.calculateNearest(points))
