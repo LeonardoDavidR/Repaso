f571c1079f158f7fd30c957456fd9260b8adc85a
Leonardo David R.M <leodar1234@gmail.com>
2023-05-09 20:34:46 +0200

    puntosxd

--- AlgebraProject/point.ts
+++ AlgebraProject/point.ts
@@ -22,5 +22,33 @@ export class Point {
     public toString(){
         return this.x+this.y;
     }
-    
+    public distanceToOrigin():number{
+        let x1 = this.x;
+        let y1 = this.y
+        let cuadrado1 = Math.pow(x1, 2);
+        let cuadrado2 = Math.pow(y1,2);
+        return Math.sqrt((cuadrado1-0) + (cuadrado2-0))
+    }
+    calculateDistance(anotherPoint:Point): number {
+        let x = this.x;
+        let y = this.y;
+        let x1 = anotherPoint.x;
+        let y1 = anotherPoint.y;
+        let restaX = x-x1;
+        let restaY = y-y1;
+        let cuadrado1 = Math.pow(restaX,2);
+        let cuadrado2 = Math.pow(restaY,2)
+        // let cuadradox = Math.pow(x, 2);
+        // let cuadradoy = Math.pow(y,2);
+        // let cuadradox1 = Math.pow(x1,2);
+        // let cuadradoy1 = Math.pow(y1,2);
+        
+        return Math.sqrt(cuadrado1+cuadrado2)
+    }
 }
+
+
+let point = new Point(7,7);
+let point2 = new Point(4,4);
+
+console.log(point.calculateDistance(point2))
